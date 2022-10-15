7d56c3e3b8cbed2e1161656d258dcfce97b53f73
gmambula <101022003+gmambula@users.noreply.github.com>
2022-10-15 18:20:20 -0300

    commit encontro remoto 4

--- PessoaJuridica.cs
+++ PessoaJuridica.cs
@@ -7,7 +7,20 @@ namespace SenaiCadastroCliente
 {
     public class PessoaJuridica : pessoa    
     {
-        public int cnpj { get; set; }
-        public string razaosocial { get; set; }
+        public string? cnpj { get; set; }   
+        public string? razaosocial { get; set; }
+        public override void Pagarimposto(float salario){
+
+        }
+        public bool validarCnpj(string cnpj){
+            if(cnpj.Length >= 14 && cnpj.Substring(cnpj.Length -6, 4) == "0001"){
+                return true;
+            }else{
+                return false;
+            }
+
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,16 +18,14 @@ namespace SenaiCadastroCliente
 
             pf.nome = "Gabriel";
             pf.endereco = end;
-            pf.enderecoComercial = false;
-            pf.cpf = 12332112345;
-            pf.datanascimaneto = 09/09/1999;
+            pf.cpf = "12332112345";
+            pf.datanascimaneto = new DateTime (1999, 01, 15);
 
             PessoaJuridica pj = new PessoaJuridica();
 
             pj.nome = "Empresax";
             pj.endereco = end;
-            pj.enderecoComercial = true;
-            pj.cnpj = 12334321000399;
+            pj.cnpj = "12334321000399";
             pj.razaosocial = "Empresa multix LTDA";
 
 
@@ -35,11 +33,19 @@ namespace SenaiCadastroCliente
             Console.WriteLine(idadeValida);
             if (idadeValida == true)
             {
-                System.Console.WriteLine{$"Cadastro aprovado"}
+                System.Console.WriteLine($"Cadastro aprovado");
             }else
             {
-                System.Console.WriteLine{$"Cadastro reprovado"}
+                System.Console.WriteLine($"Cadastro reprovado");
             }
+
+            pj.validarCnpj(pj.cnpj);
+                if(pj.validarCnpj(pj.cnpj)){
+                    Console.WriteLine("cnpj ok");
+                }else{
+                    Console.WriteLine("cnpj invalido");
+
+                }
 
         }
         
