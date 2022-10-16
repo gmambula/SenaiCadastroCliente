1df84785fb2f3934214c3fdb9b9baac3fa907ef6
gmambula <101022003+gmambula@users.noreply.github.com>
2022-10-16 11:24:59 -0300

    encontro remoto 7/8

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Threading;
+using System.Collections.Generic;
+
 namespace SenaiCadastroCliente
 {
     class Program
     {
          static void Main(string[] args)
         {
+           List<PessoaFisica> listPF = new List<PessoaFisica>();
            Console.Clear();
            Console.ForegroundColor = ConsoleColor.DarkBlue;
            Console.BackgroundColor = ConsoleColor.White;
@@ -34,12 +37,21 @@ namespace SenaiCadastroCliente
             {
                 
            Console.WriteLine(@$"
-+++++++++++++++++++++++++++++++
-|  Escolha uma opção abaixo   |
-|  1 - Pessoa Fisica          |
-|  2 - Pessoa Juridica        |
-|  0 - Sair                   |
-+++++++++++++++++++++++++++++++ 
++++++++++++++++++++++++++++++++++
+|  Escolha uma opção abaixo     |
+|     Pessoa Fisica             |
+| 1 - Cadastrar Pessoa Fisica   |
+| 2 - Listar Pessoa Fisica      |
+| 3 - Remover Pessoa Fisica     |
+|                               |
+|     Pessoa Juridica           |    
+| 4 - Cadastrar Pessoa Juridica | 
+| 5 - Listar Pessoa Juridica    |
+| 6 - Remover Pessoa Juridica   |
+|                               |
+|                               |
+|  0 - Sair                     |
++++++++++++++++++++++++++++++++++ 
 ");
 
          opcao = Console.ReadLine();
@@ -49,23 +61,54 @@ namespace SenaiCadastroCliente
                 case "1":
                  Endereco endpf = new Endereco();
 
-                 endpf.Logadouro = "rua x";
-                 endpf.Numero = 1234;
-                 endpf.Complemento = "casa";
-                 endpf.enderecoComercial = false;
+                //  endpf.Logadouro = "rua x";
+                //  endpf.Numero = 1234;
+                //  endpf.Complemento = "casa";
+                //  endpf.enderecoComercial = false;
+
+                 Console.WriteLine($"Digite seu Logadouro");
+                 endpf.Logadouro = Console.ReadLine();
+                    
+                 Console.WriteLine($"Digite seu Logadouro");
+                 endpf.Numero = int.Parse(Console.ReadLine());
+                    
+                 Console.WriteLine($"Digite seu Complemento (aperte ENTER para vazio");
+                 endpf.Complemento = Console.ReadLine();
+                    
+                 Console.WriteLine($"Este endereço é comercial? S/N");
+                 string endComercial =  Console.ReadLine().ToUpper();
+                    if (endComercial == "S"){
+                        endComercial = true;
+                    }else{
+                     endComercial = false;
+                    }
+                    
             
                  PessoaFisica pf = new PessoaFisica();
 
-                 pf.nome = "Gabriel";
-                 pf.endereco = endpf;
-                 pf.cpf = "12332112345";
-                 pf.datanascimaneto = new DateTime (1999, 01, 15);   
-                 pf.salario = 7000;               
+                //  pf.nome = "Gabriel";
+                //  pf.endereco = Console.ReadLine();                 
+                //  pf.cpf = "12332112345";
+                //  pf.datanascimaneto = new DateTime (1999, 01, 15);   
+                //  pf.salario = 7000; 
+                    
+                    Console.WriteLine($"Digite seu CPF");
+                    pf.cpf = Console.ReadLine();
+
+                    Console.WriteLine($"Digite seu nome");
+                    pf.nome = Console.ReadLine();
+                    
+                    Console.WriteLine($"Digite sua data de nascimento");
+                    pf.datanascimaneto =  DateTime.Parse(Console.ReadLine());
+                    
+
                  bool idadeValida = pf.ValidarDataNascimento(pf.datanascimaneto);
                       Console.WriteLine(idadeValida);
                         if (idadeValida == true)
                         {
                             System.Console.WriteLine($"Cadastro aprovado");
+                            listPF.Add(pf);
+                            Console.WriteLine(pf.Pagarimposto(pf.salario));
                         }else
                         {
                             System.Console.WriteLine($"Cadastro reprovado");
@@ -73,6 +116,29 @@ namespace SenaiCadastroCliente
                     break;
                 
                 case "2":
+                    foreach (var cadaItem in listPF)
+                    {
+                        Console.WriteLine($"{cadaItem.nome},{cadaItem.cpf},{cadaItem.datanascimaneto}");
+                    }
+
+                    break;
+
+                case "3":
+                    System.Console.WriteLine($"Digite o CPF que deseja remover");
+                    string cpfProcurado = Console.ReadLine();
+                    PessoaFisica pessoaEncontrada = listPF.Find(cadaItem => cadaItem.cpf == cpfProcurado);
+                    if (pessoaEncontrada != null)
+                    {
+                       listPF.Remove(pessoaEncontrada);
+                       Console.WriteLine($"pessoa removida"); 
+                    }
+                    else
+                    {
+                        Console.WriteLine($"CPF não encontrado");
+                    }
+                    break;    
+
+                case "4":
                 
                     PessoaJuridica pj = new PessoaJuridica();
 
